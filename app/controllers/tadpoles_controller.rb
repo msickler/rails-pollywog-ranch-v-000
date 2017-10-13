552159dad737d888c4048cb6e4ad7135e5e01ac9
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 20:13:56 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -1,7 +1,16 @@
 class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
-  # add your metamorphosize action here
+  def metamorphosize
+    @tadpole = Tadpole.find(params[:id])
+    @frog = Frog.new 
+    @frog.name = @tadpole.name 
+    @frog.color = @tadpole.color 
+    @frog.pond_id = @tadpole.pond_id
+    @frog.save 
+    @tadpole.destroy 
+    redirect_to @frog 
+  end 
   
   def index
     @tadpoles = Tadpole.all
